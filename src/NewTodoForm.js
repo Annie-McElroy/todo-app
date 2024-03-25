c16d86621c61a704993e2362b4cf26c3e50ae30f
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2024-03-25 11:43:15 -0500

    Form renders

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -2,10 +2,39 @@ import React, { Component } from "react";
 
 class NewTodoForm extends Component {
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { name: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    
+  }
+
+  render() {
     return (
-      <div>This is the form!</div>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="new todo">New Todo</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button>Add Todo</button>
+      </form>
     )
   };
 }
